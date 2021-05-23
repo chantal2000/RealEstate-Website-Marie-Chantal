43d4d5dcfc2b30bc735db85e2b2e1238b099cdea
chantal2000 <nniyonkuruchantal@gmail.com>
2021-05-23 17:54:11 +0300

    Real Estate Website

---
+++ ok.js
@@ -0,0 +1,6 @@
+let navWrapper = document.querySelector('.navigationBar'),
+    navToogler = document
+    .querySelector('.navigationToggle')
+navToogler.addEventListener('click', function(event) {
+    navWrapper.classList.toggle('active')
+});
